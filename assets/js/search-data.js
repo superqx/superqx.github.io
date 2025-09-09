3e68d3a5f1106749417ad4196714394259ce4b7d
superqx <48249563+superqx@users.noreply.github.com>
2025-09-09 12:44:39 +0000

    Deploying to gh-pages from @ superqx/superqx.github.io@fe6eb75e89610b2fd5aed597256016622a505c10 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -50,7 +50,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Here is the short introduction to myself.",
           section: "Navigation menu",
           handler: () => {
             window.location.href = "/cv/";
